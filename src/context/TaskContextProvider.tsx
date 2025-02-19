2af6a948ce7e88d8658e01f3ac5b21ec4c7d0b04
MahakRajwani112004 <mahak@ssipmt.com>
2025-02-19 13:30:35 +0530

    Refactor : removed uncessary code

--- src/context/TaskContextProvider.tsx
+++ src/context/TaskContextProvider.tsx
@@ -9,14 +9,6 @@ interface ITaskContextProviderProps {
 
 const TaskContext = createContext<TaskContextProps | undefined>(undefined);
 
-export const useTaskContext = () => {
-	const context = useContext(TaskContext);
-	if (!context) {
-		throw new Error('useTaskContext must be used within a TaskProvider');
-	}
-	return context;
-};
-
 export const TaskProvider = (props: ITaskContextProviderProps) => {
 	const { children } = props;
 
